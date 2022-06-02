baf61f4795de052de48a6391deb2f53b5de731c4
Felipe Del Rio <fdelrio@externos.bancoestado.cl>
2022-06-02 08:10:53 -0400

    02062022

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.enableCors({
     "origin": "*",
-    "methods": "GET,POST,OPTIONS,PUT,DELETE",
-    "allowedHeaders": "X-API-KEY, Origin, X-Requested-With, Content-Type, Access-Control-Request-Method,token,authorization,x-access-token,Authorization, Accept,X-Requested-With,content-type",
-    "credentials": true
+    "methods": ['GET', 'PUT', 'POST', 'OPTIONS', 'DELETE'],
+    "allowedHeaders": [
+      'X-API-KEY', 
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Access-Control-Request-Method',
+      'token',
+      'authorization',
+      'x-access-token',
+      'Authorization',
+      'X-Requested-With',
+      'content-type'
+    ]
   });
   await app.listen(3000);
 }
